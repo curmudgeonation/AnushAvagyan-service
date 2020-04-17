137d4efca1b1c8e36c47c8a27dadb854f4895914
Anush Avagyan <57200991+AnushAvagyan@users.noreply.github.com>
2020-04-17 01:57:33 -0700

    Add dependencies and scripts to package.json file

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  plugins: [
+    // To strip all locales except “en”
+    new MomentLocalesPlugin(),
+
+    // Or: To strip all locales except “en”, “es-us” and “ru”
+    // (“en” is built into Moment and can’t be removed)
+    new MomentLocalesPlugin({
+        localesToKeep: ['es-us', 'ru'],
+    }),
+]
+
+};
